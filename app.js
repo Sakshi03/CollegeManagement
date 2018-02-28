ded1607267515f5e93addaa8f37f0013bc7aa332
shivanubhateja <shivanubhateja31@gmail.com>
2018-02-28 10:20:23 +0530

    modal fix

--- app.js
+++ app.js
@@ -10,6 +10,32 @@ app.config(['$stateProvider' , function($stateProvider){
 }]);
 
 app.controller('myCtrl',function($scope,$http){
+    var secondBtn=document.getElementById('secondBtn');
+    var modal = document.getElementById('simpleModal');
+    var modalOne=document.getElementById('simpleModalOne');
+    var modalBtn=document.getElementById('modalBtn');
+    var closeBtn = document.getElementsByClassName('closeBtn')[0];
+    var closeBtnOne=document.getElementsByClassName('closeBtn')[1];
+    var cancelBtn=document.getElementById('cancelBtn');
+    modalBtn.addEventListener('click', openModal);
+    closeBtn.addEventListener('click', closeModal);
+    closeBtnOne.addEventListener('click',closeModalOne);
+    secondBtn.addEventListener('click',openModalOne);
+    cancelBtn.addEventListener('click',closeModalOne);
+    function openModal() {
+    console.log("aaa")
+    modal.style.display = 'block';
+    }
+    function closeModal() {
+    modal.style.display = 'none';
+    }
+    function openModalOne(){
+    modalOne.style.display='block';
+    }
+    function closeModalOne(){
+    modalOne.style.display='none';
+    }
+
   $scope.CurrentDate=new Date();
    $scope.names = ["Invalid data", "Connectivity Issue", "Lorem Ipsum"];
    $scope.getList = function(statedata) {
